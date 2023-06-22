cd770cb33d40168cf7291616ce3c7b017c143b6f
Lony <loonydarck@hotmail.com>
2023-06-22 10:32:19 -0300

    Opções de filtros de registros

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,24 @@
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/where', function (User $user) {
+    $filter = 'a';
+    //$user = $user->where('email', '=', 'kris78@example.com')->first();
+    //$user = $user->where('email', 'kris78@example.com')->first();
+    //$users = $user->where('name', 'LIKE', "%{$filter}%")->get();
+    // $users = $user->where('name', 'LIKE', "%{$filter}%")
+    //                 ->orWhere('name', 'Carlos')
+    //                 ->get();
+    $users = $user->where('name', 'LIKE', "%{$filter}%")
+                    ->orWhere(function ($query) use ($filter) {
+                        $query->where('name', '<>', 'Carlos');
+                        $query->where('name', '=', $filter);
+                    })
+                    ->toSql();
+
+    dd($users);
+});
+
 Route::get('/select', function (){
     //$users = User::all();
     //$users = User::where('id', 10)->get();
@@ -12,8 +30,6 @@ Route::get('/select', function (){
     //$user = User::findOrFail(request('id'));
     //$user = User::where('name', request('name'))->firstOrFail();
     //$user = User::firstWhere('name', request('name'));
-
-        dd($user);
 });
 
 Route::get('/', function () {
